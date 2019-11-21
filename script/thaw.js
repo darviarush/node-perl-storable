7dd1ed9b78f83561c5e0a8d06bbcb566f1a89d82
Yaroslav O. Kosmina <kosmina.yaroslav@220-volt.ru>
2019-11-21 12:06:50 +0300

    master fix spaces

--- script/thaw.js
+++ script/thaw.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
 class A {
-    constructor() {
-	    throw new Error("Create objects without constructor!");
+    constructor () {
+        throw new Error('Create objects without constructor!');
     }
 
-    getX() {
-	    return this.x;
+    getX () {
+        return this.x;
     }
 }
 
 let exec = require('child_process').execSync;
 
-let stdout = exec("perl -MStorable -e 'print Storable::freeze(bless {x=>123}, A)'");
+let stdout = exec('perl -MStorable -e \'print Storable::freeze(bless {x=>123}, A)\'');
 
-let a = require("..").thaw(stdout, {A});
+let a = require('..').thaw(stdout, { A });
 
 console.log(a instanceof A, a.getX()); // --> true, 123
 
